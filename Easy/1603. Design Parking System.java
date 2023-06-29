76187faef2d91514e66d7de8878c43c33ee03a5f
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-06-29 08:01:05 +0530

    Create 1603. Design Parking System.java

---
+++ Easy/1603. Design Parking System.java
@@ -0,0 +1,41 @@
+/* 1603. Design Parking System
+Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+
+Implement the ParkingSystem class:
+
+ParkingSystem(int big, int medium, int small) Initializes object of the ParkingSystem class. The number of slots for each parking space are given as part of the constructor.
+bool addCar(int carType) Checks whether there is a parking space of carType for the car that wants to get into the parking lot. carType can be of three kinds: big, medium, or small,
+which are represented by 1, 2, and 3 respectively. A car can only park in a parking space of its carType. If there is no space available, return false, else park the car in that size space and return true. */
+
+
+class ParkingSystem {
+    private int bigS;
+    private int mediumS;
+    private int smallS;
+    public ParkingSystem(int big, int medium, int small) {
+        bigS = big;
+        mediumS = medium;
+        smallS = small;  
+    }
+    
+    public boolean addCar(int carType) {
+         if (carType == 1 && bigS > 0) {
+            bigS--;
+            return true;
+        } else if (carType == 2 && mediumS > 0) {
+            mediumS--;
+            return true;
+        } else if (carType == 3 && smallS > 0) {
+            smallS--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
