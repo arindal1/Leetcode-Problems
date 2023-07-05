51210e25ac5f022cb85f1cea4dac5108e1e840cc
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-07-05 14:21:07 +0530

    Update 9. Palindrome Number.cpp

--- Easy/9. Palindrome Number.cpp
+++ Easy/9. Palindrome Number.cpp
@@ -1,21 +1,23 @@
 Java:
 
-if(x < 0){
-}
-
-        int reverse = 0;
-        int num=x;
-
-
-        while(x > 0){
-            reverse = reverse*10 + x%10;
-            x = x/10;
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
         }
 
-        if(reverse == num)
-          return true;
+        long reversed = 0;
+        long temp = x;
 
-        return false;
+        while (temp != 0) {
+            int digit = (int) (temp % 10);
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
+    }
+}
 
 C++:
 
