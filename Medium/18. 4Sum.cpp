3134c4a5af0548c71ca84b738de735a1667f0d63
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-07-13 22:16:16 +0530

    Update 18. 4Sum.cpp

--- Medium/18. 4Sum.cpp
+++ Medium/18. 4Sum.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        set<vector<int>> set;
-        vector<vector<int>> output;
-        for(int i=0; i<n-3; i++){
-            for(int j=i+1; j<n-2; j++){
-                for(int k=j+1; k<n-1; k++){
-                    for(int l=k+1; l<n; l++){
-                        if((long long)nums[i] + (long long)nums[j] + (long long)nums[k] + 
-                        (long long)nums[l] == target){
-                            set.insert({nums[i], nums[j], nums[k], nums[l]});
-                        }
+    vector<vector<int>> fourSum(vector<int>& nums, int t) {
+        sort(nums.begin(),nums.end());
+        set<vector<int>> st;
+        
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                int k=j+1;
+                int l=nums.size()-1;
+                
+                while(k<l){
+                    long long int sum=nums[i];
+                    sum+=nums[j];    /* to avoid RE */
+                    sum+=nums[k];
+                    sum+=nums[l];
+                    if(sum==t){
+                        st.insert({nums[i],nums[j],nums[k],nums[l]});
+                        k++;
+                        l--;
                     }
+                    else if(sum>t)
+                    l--;
+                    else
+                    k++;
                 }
             }
         }
-        for(auto it : set){
-            output.push_back(it);
-        }
-        return output;
+        vector<vector<int>> ans;
+        for(auto i:st)
+        ans.push_back(i);
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
