6b01f1fd51004ff82bfefb0d4c4b074585114b6b
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-07-22 08:12:30 +0530

    Update 70. Climbing Stairs.cpp

--- Easy/70. Climbing Stairs.cpp
+++ Easy/70. Climbing Stairs.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    void solve(int n,vector<int> &dp){
-        if(n<=2)
-          return n;
-        
-        if(dp[n]!=-1) 
-          return dp[n]; 
-        
-        dp[n]=solve(n-1)+solve(n-2);
-        return dp[n];
-    }
+
+int solve(int n,vector<int> &dp)
+{
+    if(n==0) return 1;
+    if(n<0) return 0;
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+}
     int climbStairs(int n) {
-        if(n<=2)
-         return n;
-        vector<int> dp(n+1);
-        for(int i=0;i<=n;i++)
-         dp[i]=-1;
-        
+        vector<int> dp(n+1,-1);
         return solve(n,dp);
     }
 };
