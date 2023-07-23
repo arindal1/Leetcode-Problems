946dabb32f32c43687550a572e7fa266f3c592f1
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-07-23 08:13:02 +0530

    Create 6921. Split Strings by Separator.cpp

---
+++ Contests/6921. Split Strings by Separator.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+std::vector<std::string> splitWordsBySeparator(std::vector<std::string>& words, char separator) {
+    std::vector<std::string> result;
+    for (const std::string& word : words) {
+        std::stringstream ss(word);
+        std::string temp;
+        while (std::getline(ss, temp, separator)) {
+            if (!temp.empty()) {
+                result.push_back(temp);
+            }
+        }
+    }
+    return result;
+}
+};
