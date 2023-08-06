5a731f494e50933e0e3e85848599c68b164fd101
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-08-06 09:28:45 +0530

    Create 6925. Faulty Keyboard.cpp

---
+++ Contests/6925. Faulty Keyboard.cpp
@@ -0,0 +1,41 @@
+/*
+Your laptop keyboard is faulty, and whenever you type a character 'i' on it, it reverses the string that you have written. Typing other characters works as expected.
+
+You are given a 0-indexed string s, and you type each character of s using your faulty keyboard.
+
+Return the final string that will be present on your laptop screen.
+
+ 
+
+Example 1:
+
+Input: s = "string"
+Output: "rtsng"
+Explanation: 
+After typing first character, the text on the screen is "s".
+After the second character, the text is "st". 
+After the third character, the text is "str".
+Since the fourth character is an 'i', the text gets reversed and becomes "rts".
+After the fifth character, the text is "rtsn". 
+After the sixth character, the text is "rtsng". 
+Therefore, we return "rtsng".
+  */
+
+class Solution {
+public:
+    string finalString(string s) {
+        string result = "";
+    string temp = "";
+
+    for (char c : s) {
+        if (c == 'i') {
+            reverse(temp.begin(), temp.end());
+        } else {
+            temp += c;
+        }
+    }
+
+    result = temp;
+    return result;
+    }
+};
