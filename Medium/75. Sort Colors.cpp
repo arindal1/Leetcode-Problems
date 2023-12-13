9f8472ec9989d3b5afbb4c278a23cf967866ae56
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-12-13 21:11:13 +0530

    Create 75. Sort Colors.cpp

---
+++ Medium/75. Sort Colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo=0;
+        int hi=nums.size()-1;
+        int mid=0;
+
+        while (mid<=hi)
+        {
+            if (nums[mid]==0)
+            {
+                swap(nums[lo],nums[mid]);
+                lo++;
+                mid++;
+            }
+            else if (nums[mid]==1)
+            {
+                mid++;
+            }
+            else if (nums[mid]==2)
+            {
+                swap(nums[mid],nums[hi]);
+                hi--;
+            }
+        }
+    }
+};
