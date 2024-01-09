54e1373c95603d3719efaf148ff9c429b9e73ea6
Arindal Char <110285827+arindal1@users.noreply.github.com>
2024-01-09 19:45:38 +0530

    Update 73. Set Matrix Zeroes.cpp

--- Medium/73. Set Matrix Zeroes.cpp
+++ Medium/73. Set Matrix Zeroes.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    for(int k=0; k<m; k++){
-                        if(matrix[i][k] != 0){
-                            matrix[i][k] = -9999;
-                        }
-                    }
+    void setZeroes(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<int> row(n);
+        vector<int> col(m);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    for(int k=0; k<n; k++){
-                        if(matrix[k][j] != 0){
-                            matrix[k][j] = -9999;
-                        }
-                    }
+        for(int i=0;i<n;i++){
+            if(row[i]==1){
+                for(int j=0;j<m;j++){
+                    mat[i][j]=0;
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == -9999){
-                    matrix[i][j] = 0;
+        for(int j=0;j<m;j++){
+            if(col[j]==1){
+                for(int i=0;i<n;i++){
+                    mat[i][j]=0;
                 }
             }
         }
