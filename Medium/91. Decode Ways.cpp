0748bdcfad2af724c02fb7915ca19b303fb25cbf
Arindal Char <110285827+arindal1@users.noreply.github.com>
2024-02-22 20:19:42 +0530

    updated: 91. Decode Ways.cpp

--- Medium/91. Decode Ways.cpp
+++ Medium/91. Decode Ways.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> memo(n, -1);
+        vector<int> arr(n, -1);
         function<int(int)> dp = [&](int i) {
             if (i > n || s[i] == '0')
                 return 0;
             if (i == n)
                 return 1;
-            if (memo[i] != -1)
-                return memo[i];
-            int take1 = dp(i+1);
-            int take2 = s.substr(i, 2) <= "26" ? dp(i+2) : 0;
-            return memo[i] = take1 + take2;
+            if (arr[i] != -1)
+                return arr[i];
+            int a1 = dp(i+1);
+            int a2 = s.substr(i, 2) <= "26" ? dp(i+2) : 0;
+            return arr[i] = a1 + a2;
         };
         return dp(0);
     }
