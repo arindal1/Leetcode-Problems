a304f322161708e9ffdf1990121b39e539fef806
Arindal Char <110285827+arindal1@users.noreply.github.com>
2024-05-08 16:55:46 +0530

    Update 145. Binary Tree Postorder Traversal.cpp

--- Easy/145. Binary Tree Postorder Traversal.cpp
+++ Easy/145. Binary Tree Postorder Traversal.cpp
@@ -9,37 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
+    void postorder(TreeNode *root,vector<int>& ans)
+   {
+        if(root==NULL) return;
+        postorder(root->left,ans);
+        postorder(root->right,ans);
+        ans.push_back(root->val);
+   }
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        if(root == NULL)
-            return ans;
-        stack<TreeNode*> st;
-        TreeNode* curr = root;
-        while(curr != NULL || !st.empty()) {
-            if(curr != NULL) {
-                st.push(curr);
-                curr = curr->left;
-            }
-            else {
-                TreeNode* temp = st.top()->right;
-
-                if(temp == NULL) {
-                    temp = st.top();
-                    st.pop();
-                    ans.push_back(temp->val);
-                    while(!st.empty() && temp == st.top()->right) {
-                        temp = st.top();
-                        st.pop();
-                        ans.push_back(temp->val);
-                    }
-                }
-                else {
-                    curr = temp;
-                }
-            }
-        }
+        vector<int>ans;
+        postorder(root,ans);
         return ans;
     }
 };
