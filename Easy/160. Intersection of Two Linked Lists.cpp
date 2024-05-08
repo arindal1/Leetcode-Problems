dc0b863e651802322133596cbce53e036f1e2e09
Arindal Char <110285827+arindal1@users.noreply.github.com>
2024-05-08 16:57:00 +0530

    Update 160. Intersection of Two Linked Lists.cpp

--- Easy/160. Intersection of Two Linked Lists.cpp
+++ Easy/160. Intersection of Two Linked Lists.cpp
@@ -1,33 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    int length(ListNode *head){
-        int len = 0;
-        while(head){
-            len++;
-            head = head->next;
-        }
-        return len;
-    }
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(!headA || !headB) return NULL;
-        int lenA = length(headA), lenB = length(headB);
-        if(lenA>lenB){
-            while(lenA>lenB){
-                headA = headA->next;
-                lenA--;
-            }
-        }
-        else if(lenA<lenB){
-            while(lenA<lenB){
-                headB = headB->next;
-                lenB--;
-            }
-        }
-        while(headA && headB){
-            if(headA==headB) return headA;
-            headA = headA->next;
-            headB = headB->next;
-        }
-        return NULL;
-    }
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *ptr1 = headA;
+		ListNode *ptr2 = headB;
+		while(ptr1 != ptr2){
+			if(ptr1 == NULL){
+				ptr1 = headB;
+			}
+			else{
+				ptr1 = ptr1 -> next;
+			}
+			if(ptr2 == NULL){
+				ptr2 = headA;
+			}
+			else{
+				ptr2 = ptr2 -> next;
+			}
+		}
+		return ptr1;
+	}
 };
